fecb818740be6e8d272a5253dc71652ecc3dac51
최정운 <choijung4631@naver.com>
2023-06-19 14:30:49 +0900

    홈페이지 흐르는 텍스트 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,25 +12,69 @@ function Home() {
     <>
       <Header />
       <div className="flex flex-col w-full h-[80%] justify-between">
-        <div className="flex flex-col items-center w-full h-[288px] mt-[129px] whitespace-nowrap">
-          <p className="text-[48px] text-[#1B1B1B] font-[900]">
-            말 많고 입이 멈추질 않는 프로펠러 돌아가는 소리가 들리는 사람에게
-          </p>
-          <p
+        <div className="relative mt-[129px] flex flex-col items-center w-full h-[288px] whitespace-nowrap overflow-hidden">
+          <div className="absoulte flex text-[48px] text-[#1B1B1B] font-[900] animate-flowText">
+            <div>말 많고 입이 멈추질 않는 프로펠러 돌아가는 소리가 들리는</div>
+            &nbsp;
+            <div>말 많고 입이 멈추질 않는 프로펠러 돌아가는 소리가 들리는</div>
+            &nbsp;
+            <div>말 많고 입이 멈추질 않는 프로펠러 돌아가는 소리가 들리는</div>
+            &nbsp;
+            <div>말 많고 입이 멈추질 않는 프로펠러 돌아가는 소리가 들리는</div>
+          </div>
+          <div
             style={{ textShadow: "2px 2px black" }}
-            className="text-[48px] text-white font-[900]"
+            className="absolute flex top-[25%] text-[48px] text-white font-[900] animate-flowText"
           >
-            할 거 없는데 잠 자기 보단 말하는 걸 선택하고픈 그런 날인 당신에게
-          </p>
-          <p className="text-[48px] text-[#1B1B1B] font-[900]">
-            토론을 좋아하고 자신의 주장을 내세워 이야기하길 좋아하는 여러분께
-          </p>
-          <p
+            <div>
+              할 거 없는데 잠 자기 보단 말하는 걸 선택하고픈 그런 날인 당신에게
+            </div>
+            &nbsp;
+            <div>
+              할 거 없는데 잠 자기 보단 말하는 걸 선택하고픈 그런 날인 당신에게
+            </div>
+            &nbsp;
+            <div>
+              할 거 없는데 잠 자기 보단 말하는 걸 선택하고픈 그런 날인 당신에게
+            </div>
+            &nbsp;
+            <div>
+              할 거 없는데 잠 자기 보단 말하는 걸 선택하고픈 그런 날인 당신에게
+            </div>
+          </div>
+          <div className="absolute top-[50%] flex text-[48px] text-[#1B1B1B] font-[900] animate-flowText">
+            <div>토론을 좋아하고 자신의 주장을 내세워 이야기하길 좋아하는</div>
+            &nbsp;
+            <div>토론을 좋아하고 자신의 주장을 내세워 이야기하길 좋아하는</div>
+            &nbsp;
+            <div>토론을 좋아하고 자신의 주장을 내세워 이야기하길 좋아하는</div>
+            &nbsp;
+            <div>토론을 좋아하고 자신의 주장을 내세워 이야기하길 좋아하는</div>
+          </div>
+          <div
             style={{ textShadow: "2px 2px black" }}
-            className="text-[48px] text-white font-[900]"
+            className="absolute flex top-[75%] text-[48px] text-white font-[900] animate-flowText"
           >
-            말하는 건 몰라도 말 잘하는 이에게 따봉을 들어주고픈 그런 분들에게
-          </p>
+            <div>
+              말하는 건 몰라도 말 잘하는 이에게 따봉을 들어주고픈 그런
+              분들에게도
+            </div>
+            &nbsp;
+            <div>
+              말하는 건 몰라도 말 잘하는 이에게 따봉을 들어주고픈 그런
+              분들에게도
+            </div>
+            &nbsp;
+            <div>
+              말하는 건 몰라도 말 잘하는 이에게 따봉을 들어주고픈 그런
+              분들에게도
+            </div>
+            &nbsp;
+            <div>
+              말하는 건 몰라도 말 잘하는 이에게 따봉을 들어주고픈 그런
+              분들에게도
+            </div>
+          </div>
         </div>
         <div className="flex justify-center mb-[123px]">
           <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        textLoop: {
+          "0%": {
+            transform: "translateX(0px)",
+          },
+          "100%": {
+            transform: "translateX(-25%)",
+          },
+        },
+      },
+      animation: {
+        flowText: "textLoop 8s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
